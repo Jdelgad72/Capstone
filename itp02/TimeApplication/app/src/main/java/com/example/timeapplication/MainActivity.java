2a6fad18d9d1dae01152c40cc38d1f0a40481e9b
Jdelgad <jdelgad@iu.edu>
2020-09-26 17:42:24 -0400

    Iteration 3

--- itp02/TimeApplication/app/src/main/java/com/example/timeapplication/MainActivity.java
+++ itp02/TimeApplication/app/src/main/java/com/example/timeapplication/MainActivity.java
@@ -2,27 +2,26 @@ package com.example.timeapplication;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.Calendar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView dateTimeDisplay=(TextView)findViewById(R.id.textView2);
-        Object currentDate;
-        textViewDate.setText(currentDate);
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        Date date = new Date();
-        System.out.println(dateFormat.format(date));
 
-    }
+        textView dateTimeDisplay = findViewById(R.id.textView);
+
+        Calendar calendar = Calendar.getInstance();
+
+        
+
+
+
 
 }
