6e71a8fc49af27b4426cd5f7d07b218f5b40034d
Jdelgad <jdelgad@iu.edu>
2020-09-25 22:39:42 -0400

    Iteration of time app

--- itp02/TimeApplication/app/src/main/java/com/example/timeapplication/MainActivity.java
+++ itp02/TimeApplication/app/src/main/java/com/example/timeapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.timeapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView dateTimeDisplay=(TextView)findViewById(R.id.textView2);
+        textViewDate.setText(currentDate);
     }
+
 }
