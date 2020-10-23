de72b3338e7f820184548c5efda403b7a12416b4
jdelgad <jdelgad@iu.edu>
2020-10-23 18:39:11 -0400

    first commit

--- itp04/MyPostApp/app/src/main/java/com/example/mypostapp/MainActivity.java
+++ itp04/MyPostApp/app/src/main/java/com/example/mypostapp/MainActivity.java
@@ -6,8 +6,17 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import java.text.BreakIterator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +35,3 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
-    private void postRequest() {
-        RequestQueue requestQueue= Volley.newRequestQueue( MainActivity.this);
-    }
-
-}
