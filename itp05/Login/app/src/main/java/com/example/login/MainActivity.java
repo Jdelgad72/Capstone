1f703b36df41b93346fbb9d71ef8f51316d85871
jdelgad <jdelgad@iu.edu>
2020-11-08 20:04:08 -0500

    Running out of time, need to find a way to connect DB

--- itp05/Login/app/src/main/java/com/example/login/MainActivity.java
+++ itp05/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -16,4 +16,40 @@ public class MainActivity extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private int counter = 5;
-    
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Name = findViewById(R.id.etName);
+        Password = findViewById(R.id.etPassword);
+        Info = findViewById(R.id.tvInfo);
+        Login = findViewById(R.id.btnSubmit);
+
+        Info.setText("Number of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void validate(String userName, String userPassword) {
+        if((userName.equals("Admin")) && userPassword.equals("1234")) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("Number of attempts remaining: " + counter);
+
+            if(counter == 0) {
+                Login.setEnabled(false);
+            }
+
+        }
+    }
+}
